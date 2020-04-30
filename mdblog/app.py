b0bdf749609b3b249ad42f820df4e5f976572cf3
mscerba <scerba4@gmail.com>
2020-04-30 12:56:20 +0000

    using jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from .database import articles
 
 
 
@@ -13,10 +14,18 @@ def view_welcome_page():
 def view_about():
     return render_template("about.html", text="Toto je text")
 
-@flask_app.route("/articles/")
-def view_articles():
-    return render_template("articles.html", text="Toto je text")
-
 @flask_app.route("/admin/")
 def view_admin():
     return render_template("admin.html", text="Toto je text")
+
+@flask_app.route("/articles/")
+def view_articles():
+    return render_template("articles.html", articles=articles.items())
+
+@flask_app.route("/articles/<int:art_id>")
+def view_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.html", article=article)
+    return render_template("article_not_found.html", art_id=art_id)
+
---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+            1 : {
+                "title" : "První článek",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                },
+            2 : {
+                "title" : "Druhý článek",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                },
+            3 : {
+                "title" : "Třetí článek",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                },
+            4 : {
+                "title" : "Čtvrtý článek",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                }
+        }
