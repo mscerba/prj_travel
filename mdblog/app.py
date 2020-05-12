efbabff4daf8f175968ca0dc5fcb14c46e7755fc
mscerba <scerba4@gmail.com>
2020-05-12 07:30:54 +0000

    add + edit article

--- mdblog/app.py
+++ mdblog/app.py
@@ -5,6 +5,16 @@ from flask import redirect
 from flask import url_for
 from flask import session
 from flask import g
+from flask import flash
+
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import PasswordField
+from wtforms import TextAreaField
+from wtforms.validators import InputRequired
+
+
+
 from flask import url_for
 import sqlite3
 import os
@@ -15,6 +25,18 @@ flask_app.config.from_pyfile("/vagrant/configs/default.py")
 
 if "MDBLOG_CONFIG" in os.environ:
     flask_app.config.from_envvar("MDBLOG_CONFIG")
+
+## FORMS
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = StringField("Password", validators=[InputRequired()])
+
+class ArticleForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired()])
+    content = TextAreaField("content")
+
+## CONTROLLERS
 
 @flask_app.route("/")
 def view_welcome_page():
@@ -27,8 +49,11 @@ def view_about():
 @flask_app.route("/admin/")
 def view_admin():
     if "logged" not in session:
+        flash("You must be logged in", "alert-danger")
         return redirect(url_for("view_login"))
     return render_template("admin.html", text="Toto je text")
+
+### ARTICLES
 
 @flask_app.route("/articles/", methods=["GET"])
 def view_articles():
@@ -37,12 +62,25 @@ def view_articles():
     articles = cur.fetchall()
     return render_template("articles.html", articles=articles)
 
+@flask_app.route("/articles/new/", methods=["GET"])
+def view_add_article():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+    
+    form=ArticleForm()
+    return render_template("article_editor.html", form=form)
+    
+
 @flask_app.route("/articles/", methods=["POST"])
 def add_article():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+
     db = get_db()
     db.execute("insert into articles (title, content) values (?,?)",
                [request.form.get("title"), request.form.get("content")])
     db.commit()
+    flash("Article was saved", "alert-success")
     return redirect(url_for("view_articles"))
 
 
@@ -55,23 +93,73 @@ def view_article(art_id):
         return render_template("article.html", article=article)
     return render_template("article_not_found.html", art_id=art_id)
 
+@flask_app.route("/articles/<int:art_id>/edit/", methods=["GET"])
+def view_article_editor(art_id):
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+    db = get_db()
+    cur = db.execute("select * from articles where id=(?)", [art_id])
+    article = cur.fetchone()
+    if article:
+        form = ArticleForm()
+        form.title.data = article["title"]
+        form.content.data = article["content"]
+        return render_template("article_editor.html", form=form, article=article)
+    return render_template("article_not_found.html", art_id=art_id)
+
+
+@flask_app.route("/articles/<int:art_id>/", methods=["POST"])
+def edit_article(art_id):
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
+    db = get_db()
+    cur = db.execute("select * from articles where id=(?)", [art_id])
+    article = cur.fetchone()
+    if article:
+        edit_form = ArticleForm(request.form)
+        if edit_form.validate():
+            db.execute("update articles set title=?, content=? where id=?", 
+            [edit_form.title.data, edit_form.content.data, art_id])
+            db.commit()
+            flash("Edit saved", "alert-success")
+            return redirect(url_for("view_articles", art_id=art_id))
+        else:
+            for error in login_form.errors:
+                flash("{} is missing".format(error), "alert-danger")
+            return redirect(url_for("view_login"))
+
+
+
+
+
+
 @flask_app.route("/login/", methods=["GET"])
 def view_login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    return render_template("login.html", form=login_form)
 
 @flask_app.route("/login/", methods=["POST"])
 def login_user():
+    login_form = LoginForm(request.form)
     username = request.form["username"]
     password = request.form["password"]
-    if username == flask_app.config["USERNAME"] and password == flask_app.config["PASSWORD"]:
-        session["logged"] = True
-        return redirect(url_for("view_admin"))
+    if login_form.validate():
+        if login_form.username.data == flask_app.config["USERNAME"] and login_form.password.data == flask_app.config["PASSWORD"]:
+            session["logged"] = True
+            flash("Login successful", "alert-success")
+            return redirect(url_for("view_admin"))
+        else:
+            flash("Invalid credentials", "alert-danger")
+            return render_template("login.html", form=login_form)
     else:
+        for error in login_form.errors:
+            flash("{} is missing".format(error), "alert-danger")
         return redirect(url_for("view_login"))
 
 @flask_app.route("/logout/", methods=["POST"])
 def logout_user():
     session.pop("logged")
+    flash("Logout successful", "alert-success")
     return redirect(url_for("view_welcome_page"))
 
 ## funkce pomocí které se budu připojovat na databázi
